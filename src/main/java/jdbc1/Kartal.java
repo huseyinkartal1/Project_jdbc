bcedafe8ac95359f0ada3e3423325b12d0223366
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-10-27 19:30:21 +0300

    Kartal class added

--- src/main/java/jdbc1/Kartal.java
+++ src/main/java/jdbc1/Kartal.java
@@ -16,26 +16,41 @@ public class Kartal {
 
 
 
-        String sql="SELECT * FROM doctors";
 
-        rs = statement.executeQuery(sql);
+        String sql="SELECT * FROM kartal";
+        String sql2="DELETE FROM kartal WHERE Adı IS NULL";
+        String sql3="DELETE FROM kartal WHERE Soyadı IS NULL";
+        String sql4="SELECT * FROM kartal WHERE Sehir LIKE 'İ%'";
+        String sql5="SELECT COUNT(*) FROM kartal WHERE yas = 20";
+
+        //  statement.executeQuery(sql3);
+
+        statement.execute(sql3);
+        rs = statement.executeQuery(sql5);
 
         ResultSetMetaData rsmd = rs.getMetaData();
 
+
+
         int col=rsmd.getColumnCount();
+        for (int i = 1; i <= col; i++) {
+            System.out.printf("%-10s ",rsmd.getColumnName(i));
+        }
+        System.out.println();
+        System.out.println("-".repeat(50));
 
         while (rs.next()){
             for (int i = 1; i <= col; i++) {
-                System.out.println(rs.getString(i));
+
+                System.out.printf("%-10s ", rs.getString(i));
             }
-
-
+            System.out.println();
         }
     }
 
     @BeforeTest
     public void beforeClass() throws SQLException {
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306",
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema",
                 "root",
                 "rootpass123");
         statement=conn.createStatement();
